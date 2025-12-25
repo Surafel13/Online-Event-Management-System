5e9da1176e6455b2a77ae2f8d12a7562e6640007
Frezer-G <207617995+Frezer-G@users.noreply.github.com>
2025-12-25 23:08:42 +0300

    Create EventDAO class and imports

---
+++ BackEnd/src/main/java/com/eventmgmt/dao/EventDAO.java
@@ -0,0 +1,10 @@
+package com.eventmgmt.dao;
+
+import com.eventmgmt.models.Event;
+import com.eventmgmt.utils.DBUtils;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventDAO {
+}
