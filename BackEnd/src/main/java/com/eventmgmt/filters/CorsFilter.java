38fa6dabbb3057ee12e6eb6f559c6f0cd1703626
unknown <tesfayesurafel18@gmail.com>
2025-12-25 14:24:12 -1100

    Preparing cors filter file for the cors policy implementation

---
+++ BackEnd/src/main/java/com/eventmgmt/filters/CorsFilter.java
@@ -0,0 +1,7 @@
+package com.eventmgmt.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
