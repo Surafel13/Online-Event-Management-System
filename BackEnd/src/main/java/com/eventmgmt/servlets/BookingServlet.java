ded4f310fb84b33ad73b7a7c70086b6bdff13fbb
Betelhem Hailu <hailubetelhem3@gmail.com>
2025-12-29 00:03:07 +0300

    Initialize BookingServlet and required imports

---
+++ BackEnd/src/main/java/com/eventmgmt/servlets/BookingServlet.java
@@ -0,0 +1,15 @@
+package com.eventmgmt.servlets;
+
+import com.eventmgmt.dao.EventDAO;
+import com.eventmgmt.dao.TicketDAO;
+import com.eventmgmt.models.Event;
+import com.eventmgmt.models.Ticket;
+import com.eventmgmt.models.User;
+import com.google.gson.Gson;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.UUID;
